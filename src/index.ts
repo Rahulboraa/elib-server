659702b9ec6d5820e862a488b2b3f61957175a1e
Rahul Bora <rahulbora101@gmail.com>
2025-05-05 21:38:37 +0530

    removed dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("server Running");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on ${PORT}`);
+});
